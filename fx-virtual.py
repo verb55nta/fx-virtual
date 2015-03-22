1fa5cb3ae046e5802e78201fa79bc61679781852
verb55-windows <verb55nta@gmail.com>
2015-03-23 01:07:58 +0900

    initして何回も値が初期化されるバグを直した

--- fx-virtual.py
+++ fx-virtual.py
@@ -2,21 +2,20 @@
 
 import datetime
 import sys
+import os.path
 
-def fx_init():
-    now = datetime.datetime.today()
-    
-    
-    yen_file = open('yen','a+')
-    line = yen_file.readline()
-    if line == "":
-        yen_file.write("{0},1000000".format(now.strftime("%Y/%m/%d %H:%M:%S")))
-    doller_file = open('doller','a+')
-    line = doller_file.readline()
-    if line == "":
-            doller_file.write("{0},0".format(now.strftime("%Y/%m/%d %H:%M:%S")))
-            
-    yen_file.close()
-    doller_file.close()
+class Fx:
+    def __init__(self,yen,doller):
+        now = datetime.datetime.today()
+        yen_file = open('./yen','a+')
+        if os.path.getsize("./yen") == 0:
+            yen_file.write("{0},{1}".format(now.strftime("%Y/%m/%d %H:%M:%S"),yen))
+        doller_file = open('doller','a+')
+        line = doller_file.readlines()
+        if os.path.getsize("./doller") == 0:
+            doller_file.write("{0},{1}".format(now.strftime("%Y/%m/%d %H:%M:%S"),doller))
+        yen_file.close()
+        doller_file.close()
 
-fx_init()
+p=Fx(2000000,0)
+p
