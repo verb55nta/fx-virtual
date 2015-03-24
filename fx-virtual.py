b3e6ce8fd5a7c476ae3bf5bbac3ec624a300ce64
verb55 <verb.storage.in.net@gmail.com>
2015-03-24 13:08:00 +0900

    init時にプライベート変数に現在の円、ドルを保存した

--- fx-virtual.py
+++ fx-virtual.py
@@ -6,6 +6,11 @@ import os.path
 
 class Fx:
     def __init__(self,yen,doller):
+        
+        self.c_place = 1
+        self.yen = 0
+        self.doller = 0
+        
         now = datetime.datetime.today()
         yen_file = open('./yen','a+')
         if os.path.getsize("./yen") == 0:
@@ -14,6 +19,22 @@ class Fx:
         line = doller_file.readlines()
         if os.path.getsize("./doller") == 0:
             doller_file.write("{0},{1}".format(now.strftime("%Y/%m/%d %H:%M:%S"),doller))
+
+        yen_file.seek(0)
+        line=yen_file.readline()
+        while line:
+            self.yen = int(line.split(',')[self.c_place])
+            line=yen_file.readline()
+        
+        doller_file.seek(0)
+        line=doller_file.readline()
+        while line:
+            self.doller = int(line.split(',')[self.c_place])
+            line=doller_file.readline()
+        
+        print("yen={0}".format(self.yen))
+        print("doller={0}".format(self.doller))
+
         yen_file.close()
         doller_file.close()
 
