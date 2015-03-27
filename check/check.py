da2b287cc5f164e7d97e496d0f4d40eaaf78cd2e
verb55 <verb.storage.in.net@gmail.com>
2015-03-27 16:59:34 +0900

    check.py:able to read now date file

--- check/check.py
+++ check/check.py
@@ -6,3 +6,10 @@ import os
 import os.path
 import math
 
+now = datetime.datetime.today()
+data_path_file = open("./data_path_doller","r")
+path=data_path_file.read().rstrip("\n")
+#print(path)
+#print(now.strftime("%Y%m%d"))
+doll_rate_file=open(path+now.strftime("%Y%m%d"))
+print(doll_rate_file.read())
